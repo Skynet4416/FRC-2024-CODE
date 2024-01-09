a8d3199be35efe64a55a6b0437fbad5b07fe2909
NufarLA <amiaznuf@gmail.com>
2024-01-09 21:04:24 +0200

    opened a vision branch and did very little auto
    there is a very basic vision subsystem, we'll need to use it later. we didn't get to write much for auto.
    
    Co-Authored-By: ido947373848209477482846379293 <152088280+ido947373848209477482846379293@users.noreply.github.com>

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,13 +5,15 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Drive.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  //Example static factory for an autonomous command.
+  public static Command DriveAuto(DriveSubsystem driveSubsystem) {
+    //todo: change to actual auto
+    //return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
 
   private Autos() {
--- src/main/java/frc/robot/subsystems/vision/visionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/visionSubsystem.java
@@ -1,5 +1,48 @@
 package frc.robot.subsystems.vision;
+import org.photonvision.PhotonCamera;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import java.util.List;
 
-public class visionSubsystem {
+public class VisionSubsystem extends SubsystemBase
+{
+    private final PhotonCamera m_camera;
+    boolean hasTarget; // Stores whether or not a target is detected
+    PhotonPipelineResult result; // Stores all the data that Photonvision returns
+
+    public VisionSubsystem()
+    {
+        this.m_camera = new PhotonCamera("Aizen");
+    }
+
+    @Override
+    public void periodic() {
+        PhotonPipelineResult result = m_camera.getLatestResult(); // Query the latest result from PhotonVision
+        hasTarget = result.hasTargets(); // If the camera has detected an apriltag target, the hasTarget boolean will be true
+        if (hasTarget) {
+            this.result = result;
+        }
+    }
+    public PhotonTrackedTarget getTargetWithID(int id) { // Returns the apriltag target with the specified ID (if it exists)
+        List<PhotonTrackedTarget> targets = result.getTargets(); // Create a list of all currently tracked targets
+        for (PhotonTrackedTarget i : targets) {
+            if (i.getFiducialId() == id) { // Check the ID of each target in the list
+                return i; // Found the target with the specified ID!
+            }
+        }
+        return null; // Failed to find the target with the specified ID
+    }
     
+    public PhotonTrackedTarget getBestTarget() {
+        if (hasTarget) {
+        return result.getBestTarget(); // Returns the best (closest) target
+        }
+        else {
+            return null; // Otherwise, returns null if no targets are currently found
+        }
+    }
+    public boolean getHasTarget() {
+        return hasTarget; // Returns whether or not a target was found
+    }
 }

9abab14ffcc3f12d89e4041bf8c68500f2b698a9
NufarLA <amiaznuf@gmail.com>
2024-01-15 18:23:16 +0200

    prototype branch
    a branch with nothing but what is needed to move the prototype

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,10 @@
 
 package frc.robot;
 
-import frc.robot.subsystems.Drive.DriveSubsystem;
+// import frc.robot.subsystems.Drive.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.DriveCommand;
+// import frc.robot.commands.DriveCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,7 +19,7 @@ public class RobotContainer {
   // ? https://www.chiefdelphi.com/t/why-do-many-teams-put-a-m-in-front-of-many-variable-names/377126
   // ? this is why i put m_(variable name)
   // The robot's subsystems and commands are defined here...
-  private final DriveSubsystem m_driveSubsystem;
+  // private final DriveSubsystem m_driveSubsystem;
   private final OI oi;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -28,15 +28,15 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    this.m_driveSubsystem = new DriveSubsystem();
-    this.oi = new OI();
-    configureBindings();
-    m_driveSubsystem.setAllModulesToZero();
+  //   this.m_driveSubsystem = new DriveSubsystem();
+        this.oi = new OI();
+       configureBindings();
+  //   m_driveSubsystem.setAllModulesToZero();
   }
 
-  public DriveSubsystem getDriveSubsystem(){
-    return m_driveSubsystem;
-  }
+  // public DriveSubsystem getDriveSubsystem(){
+  //   return m_driveSubsystem;
+  // }
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
@@ -49,7 +49,7 @@ public class RobotContainer {
   private void configureBindings() 
   {  
       // m_driveSubsystem.setDefaultCommand(new DriveCommand(m_driveSubsystem, oi.joystickLeft::getX, oi.joystickLeft::getY, oi.joystickRight::getX));
-      m_driveSubsystem.setDefaultCommand(new DriveCommand(m_driveSubsystem, oi.xboxController::getLeftX, oi.xboxController::getLeftY, oi.xboxController::getRightX));
+      // m_driveSubsystem.setDefaultCommand(new DriveCommand(m_driveSubsystem, oi.xboxController::getLeftX, oi.xboxController::getLeftY, oi.xboxController::getRightX));
   }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
---
+++ src/main/java/frc/robot/commands/Auto.java
@@ -0,0 +1,20 @@
+// package frc.robot.commands;
+
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.subsystems.Drive.DriveSubsystem;
+// import java.util.HashMap;
+// import java.util.Map;
+
+// public class Auto extends Command {
+//     //will contain all of the commands 
+//     private static HashMap<String, Command> eventMap;
+//     private final DriveSubsystem m_driveSubsystem;
+
+//     //add vision later
+//     public Auto(DriveSubsystem driveSubsystem)
+//     {
+//         this.m_driveSubsystem = driveSubsystem;
+        
+//         addRequirements(driveSubsystem);
+//     }
+// }

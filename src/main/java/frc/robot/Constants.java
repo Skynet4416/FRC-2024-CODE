e02cfdeb57894dad465f516380055dfcfb0f42b1
tamirco2003 <tamir.cohen835@gmail.com>
2024-03-01 16:53:19 +0200

    Add Arm kD to make is behave better.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,7 +58,7 @@ public final class Constants {
         public static class Pid {
             public static final double kP = 0.1;
             public static final double kI = 0;
-            public static final double kD = 0;
+            public static final double kD = 0.1;
         }
 
     }
